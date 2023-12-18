2f1121eeb2d48aa9ab2231eb44ab5b72eac7ab3b
Стас naprienko ekaterina <catrit1993@gmail.com>
2023-12-18 17:51:26 +0500

    add magic methods

--- src/channel.py
+++ src/channel.py
@@ -16,9 +16,31 @@ class Channel:
         self.title = channel['items'][0]['snippet']['title']
         self.description = channel['items'][0]['snippet']['description']
         self.url = channel['items'][0]['snippet']['thumbnails']['default']['url']
-        self.subscriber_count = channel['items'][0]['statistics']['subscriberCount']
+        self.subscriber_count = int(channel['items'][0]['statistics']['subscriberCount'])
         self.video_count = channel['items'][0]['statistics']['videoCount']
         self.view_count = channel['items'][0]['statistics']['viewCount']
+
+
+    def __str__(self):
+        return f'{self.title} ({self.url})'
+
+    def __add__(self, other):
+        return self.subscriber_count + other.subscriber_count
+
+    def __sub__(self, other):
+        return self.subscriber_count - other.subscriber_count
+
+    def __lt__(self, other):
+        return self.subscriber_count < other.subscriber_count
+
+    def __le__(self, other):
+        return self.subscriber_count <= other.subscriber_count
+
+    def __gt__(self, other):
+        return self.subscriber_count > other.subscriber_count
+
+    def __ge__(self, other):
+        return self.subscriber_count >= other.subscriber_count
 
     def print_info(self) -> None:
         """Выводит в консоль информацию о канале."""
